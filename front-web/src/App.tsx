695dfea56a71324f503917cc607f70c69db76e98
rensilver <ren.oliv87@gmail.com>
2021-01-08 22:20:00 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import NavBar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
-    <div>
+    <>
       <NavBar />
-    </div>
+      <Home />
+    </>
   );
 }
 
